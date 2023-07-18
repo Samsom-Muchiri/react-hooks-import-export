fb458b081938515f140541702956f5d622c5b9d0
Sam .M <you@example.com>
2023-07-18 11:07:47 +0300

    cplt

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,7 +1,9 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
+  console.log(MesaVerde);
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
